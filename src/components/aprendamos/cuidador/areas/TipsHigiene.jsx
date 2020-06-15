31c7c07c99379c1d2ce812d7040330c0e0db57f5
NardaC <nardac6@gmail.com>
2020-06-15 16:37:47 -0500

    tips

--- src/components/aprendamos/cuidador/areas/TipsHigiene.jsx
+++ src/components/aprendamos/cuidador/areas/TipsHigiene.jsx
@@ -75,7 +75,7 @@ const TipsHigiene = ({
                     <div className="row tips-margin-top sin-margin-row tips-1-columna">
                       <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
                         {item.data().icono !== "" ? (
-                          <img src={item.data().icono} alt="" className="iconos-tips-blanco"/>
+                          <img src={item.data().icono} alt="" className="iconos-tips-blanco heartbeat"/>
                         ) : (
                           <div className="videoWrapper">
                             <iframe
@@ -142,7 +142,7 @@ const TipsHigiene = ({
                     <div className="row tips-margin-top sin-margin-row tips-1-columna">
                       <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6">
                         {item.data().icono !== "" ? (
-                          <img src={item.data().icono} alt="" />
+                          <img src={item.data().icono} alt="" className="iconos-tips-blanco heartbeat"/>
                         ) : (
                           <div className="videoWrapper">
                             <iframe
