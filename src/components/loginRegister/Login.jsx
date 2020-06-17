734845da5b39b505bb488d795f89579c4f307ef4
NardaC <nardac6@gmail.com>
2020-06-17 04:13:04 -0500

    data

--- src/components/loginRegister/Login.jsx
+++ src/components/loginRegister/Login.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./loginRegister.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import arrowLeft from "./../../images/arrow-left-blue.svg";
 import arrowRight from "./../../images/arrow-right-blue.svg";
 import {auth, firebase, db} from "../firebase/firebase";
 import { withRouter } from "react-router-dom";
+
 const Login = (props) => {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
