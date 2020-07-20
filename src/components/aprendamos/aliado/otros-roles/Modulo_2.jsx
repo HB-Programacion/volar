bea3d11b75629550ebdb80ee0703929551250aa5
NardaC <nardac6@gmail.com>
2020-07-20 18:20:52 -0500

    modulos

--- src/components/aprendamos/aliado/otros-roles/Modulo_2.jsx
+++ src/components/aprendamos/aliado/otros-roles/Modulo_2.jsx
@@ -1,9 +1,111 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
+
+import flecha from "../../../../images/modulos-img/flecha-abajo.svg"
+import cerebro from "../../../../images/modulos-img/cerebro.svg"
+import calidad from "../../../../images/modulos-img/calidad.svg"
+import estres from "../../../../images/modulos-img/estres.svg"
+import infografia from "../../../../images/modulos-img/infografia.svg"
+import video from "../../../../images/modulos-img/video.svg"
+import arrowLeft from "./../../../../images/arrow-left-blue.svg";
+import arrowRight from "./../../../../images/arrow-right-blue.svg";
+
 
 const Modulo_2 = () => {
+    let history = useHistory();
     return (
-        <div>
-            
+        <div className="box-modulos-aliados">
+         <i onClick={()=>{history.goBack()}} className="fas fa-chevron-left arrowGoback-docente goBack-docente" aria-hidden="true"></i>
+            <div className="row sin-margin-row">
+                <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6 box-modulos-textos">
+                    <h3 className="title-modulos-aliados">¿Qué aprenderemos?</h3>
+                    <p className="p-mobile-blue">Establecer una relación directa entre salud y desarrollo infantil temprano.</p>
+                    <p className="p-mobile-blue">Emplear estrategias para promover la lactancia materna exclusiva, prolongada y el destete.</p>
+                    <p className="p-mobile-blue">Comprender la importancia de asistir al establecimiento de salud para orientar a las familias que deben llevar a su niño al Control de Crecimiento y Desarrollo (CRED).</p>
+                    <p className="p-mobile-blue">Manejar los mensajes sobre los mitos y efectos secundarios para asegurar el consumo de suplemento de hierro de los niños en edad de consumo.</p>
+                </div>
+                <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6 box-modulos-textos">
+                    <h3 className="title-modulos-aliados">¡No olvidar! </h3>
+                    <p className="p-mobile-blue">Los 5 primeros años de vida son fundamentales para el desarrollo integral de las personas ya que se consolida la arquitectura básica del cerebro.</p>
+                    <p className="p-mobile-blue">Las interacciones positivas con nuestros niños son clave para garantizar el desarrollo de su cerebro. </p>
+                    <p className="p-mobile-blue">Estar atento a las miradas, gestos, balbuceos, palabras y movimientos del niño para responder con cariño y de manera oportuna. Si el niño pregunta, el cuidador principal debe responder mirando a los ojos, con atención, cariño y paciencia a sus inquietudes.</p>
+                </div>
+            </div>
+            <figure className="flecha-abajo-aliado">
+                <img src={flecha} alt="" className="w-100 m-auto"/>
+            </figure>
+            <h2 className="text-materiales-aliados">Materiales</h2>
+            <div className="row sin-margin-row box-materiales-modulos">
+                <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 ">
+                    <div>
+                        <figure className="w-20 m-auto">
+                            <img src={cerebro} alt="" className="w-100"/>
+                        </figure>
+                        <h3 className="title-materiales-modulos">Lactancia Materna Exclusiva</h3>
+                        <p className="subtitle-materiales-modulos">Durante los 06 primeros meses, los bebés solo deben tomar leche materna.  </p>
+                    </div>
+                    <div className="btn-modulos-info">
+                        <figure className="btn-infografia">
+                            <img src={infografia} alt=""className="w-100"/>
+                        </figure>
+                        <div className="btn-info-text">Infografía: beneficios de la lactancia materna en la salud del niño</div>
+                    </div>
+                    <div className="btn-modulos-info">
+                        <figure className="btn-infografia">
+                            <img src={infografia} alt="" className="w-100"/>
+                        </figure>
+                        <div className="btn-info-text">Infografía: posturas/posiciones para la lactancia</div>
+                    </div>
+                </div>
+                <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 ">
+                    <div>
+                        <figure className="w-20 m-auto">
+                            <img src={calidad} alt="" className="w-100"/>
+                        </figure>
+                        <h3 className="title-materiales-modulos">CRED</h3>
+                        <p className="subtitle-materiales-modulos">Motiva a los cuidadores que visitas a que  asistan a sus controles CRED. </p>
+                    </div>
+                    <div className="btn-modulos-info">
+                        <figure className="btn-infografia">
+                            <img src={infografia} alt=""className="w-100"/>
+                        </figure>
+                        <div className="btn-info-text">Infografía: importancia del CRED</div>
+                    </div>
+                    <div className="btn-modulos-info">
+                        <figure className="btn-infografia">
+                            <img src={infografia} alt="" className="w-100"/>
+                        </figure>
+                        <div className="btn-info-text">Inforgrafía: CRED según edad de nacido</div>
+                    </div>
+                </div>
+                <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 ">
+                    <div>
+                        <figure className="w-20 m-auto">
+                            <img src={estres} alt="" className="w-100"/>
+                        </figure>
+                        <h3 className="title-materiales-modulos">Suplementos de hierro</h3>
+                        <p className="subtitle-materiales-modulos">Acompaña a las familias en su experiencia con los suplementos de hierro. </p>
+                    </div>
+                    <div className="btn-modulos-info">
+                        <figure className="btn-infografia">
+                            <img src={infografia} alt=""className="w-100"/>
+                        </figure>
+                        <div className="btn-info-text">Infografía: ¿Cómo afecta la anemia a los niños?: mala alimentación, influye en el desarrollo físico y emocional</div>
+                    </div>
+                    <div className="btn-modulos-info">
+                        <figure className="btn-infografia">
+                            <img src={infografia} alt=""className="w-100"/>
+                        </figure>
+                        <div className="btn-info-text">Infografía: pasos para administrar el sulfato ferrosoen los niños</div>
+                    </div>
+                    <div className="btn-modulos-info">
+                        <figure className="btn-infografia">
+                            <img src={infografia} alt=""className="w-100"/>
+                        </figure>
+                        <div className="btn-info-text">Infografía: efectos secundarios de los suplementos de hierro.</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/aprendamos/aliado/otros-roles/Modulo_4.jsx
+++ src/components/aprendamos/aliado/otros-roles/Modulo_4.jsx
@@ -1,9 +1,85 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
+
+import flecha from "../../../../images/modulos-img/flecha-abajo.svg"
+import cerebro from "../../../../images/modulos-img/cerebro.svg"
+import calidad from "../../../../images/modulos-img/calidad.svg"
+import estres from "../../../../images/modulos-img/estres.svg"
+import infografia from "../../../../images/modulos-img/infografia.svg"
+import video from "../../../../images/modulos-img/video.svg"
+import arrowLeft from "./../../../../images/arrow-left-blue.svg";
+import arrowRight from "./../../../../images/arrow-right-blue.svg";
+
 
 const Modulo_4 = () => {
+    let history = useHistory();
     return (
-        <div>
-            
+        <div className="box-modulos-aliados">
+         <i onClick={()=>{history.goBack()}} className="fas fa-chevron-left arrowGoback-docente goBack-docente" aria-hidden="true"></i>
+            <div className="row sin-margin-row">
+                <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6 box-modulos-textos">
+                    <h3 className="title-modulos-aliados">¿Qué aprenderemos?</h3>
+                    <p className="p-mobile-blue">Formular y practicar estrategias para 
+incrementar las oportunidades de 
+aprendizaje en comunicación, desarrollo 
+emocional y vínculo con los niños durante 
+la rutina.</p>
+                    <p className="p-mobile-blue">Construir estrategias que fomenten la 
+autonomía durante los momentos de 
+cuidado que le brindamos a los niños.</p>
+                    <p className="p-mobile-blue">Identificar el rol de los adultos en los 
+momentos de rutina y cuidado.</p>
+                    <p className="p-mobile-blue">Desarrollar la habilidad de responder al 
+niño, a través de la comunicación verbal, 
+reconociendo la importancia de llamar al 
+niño por su nombre y tener un trato 
+afectuoso en los momentos de cuidado.</p>
+                </div>
+                <div className="col-sm-12 col-md-12 col-lg-6 col-xl-6 box-modulos-textos">
+                    <h3 className="title-modulos-aliados">¡No olvidar! </h3>
+                    <p className="p-mobile-blue">Recuerda anticipar y verbalizar cada 
+acción que realices durante los distinto 
+momentos de cuidado.</p>
+                    <p className="p-mobile-blue">Contarles cuentos a los niños ayuda 
+desarrollar su cerebro y es un momento 
+de compartir emociones, también 
+permítele que cuente la historia a su 
+modo.</p>
+                    <p className="p-mobile-blue">Según la edad de nuestros niños 
+podemos invitarlos a participar en las 
+actividades cotidianas del hogar.</p>
+                </div>
+            </div>
+            <figure className="flecha-abajo-aliado">
+                <img src={flecha} alt="" className="w-100 m-auto"/>
+            </figure>
+            <h2 className="text-materiales-aliados">Materiales</h2>
+            <div className="row sin-margin-row box-materiales-modulos">
+                <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 m-auto">
+                    <div>
+                        <figure className="w-20 m-auto">
+                            <img src={cerebro} alt="" className="w-100"/>
+                        </figure>
+                        <h3 className="title-materiales-modulos">Rutinas</h3>
+                        <p className="subtitle-materiales-modulos">Infografía: beneficios de 
+las rutinas para los niños </p>
+                    </div>
+                    <div className="btn-modulos-info">
+                        <figure className="btn-infografia">
+                            <img src={infografia} alt=""className="w-100"/>
+                        </figure>
+                        <div className="btn-info-text">Infografía: pasos para 
+crear una rutina diaria</div>
+                    </div>
+                    <div className="btn-modulos-info">
+                        <figure className="btn-infografia">
+                            <img src={infografia} alt="" className="w-100"/>
+                        </figure>
+                        <div className="btn-info-text">Infografía: posturas/posiciones para la lactancia</div>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     )
 }
