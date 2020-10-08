0949b6c160fcc35d4ed478488a9f5c8c90e6e8e7
NardaC <nardac6@gmail.com>
2020-10-08 12:43:22 -0500

    volar new

--- src/components/aprendamos/cuidador/areas/TipsSesamo.jsx
+++ src/components/aprendamos/cuidador/areas/TipsSesamo.jsx
@@ -3,85 +3,72 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./tips.css";
 import { useHistory } from "react-router-dom";
 /*import cohete from '../../images/cohete-volando.svg';*/
-import arrTips from '../../../../data'
+import arrTips from "../../../../data";
 
-const TipsSesamo = ({
-  contenidoFirebase,
-  error,
-  loading,
-}) => {
-
+const TipsSesamo = ({ contenidoFirebase, error, loading }) => {
   let history = useHistory();
- /*  const childData= db.collection("usuarios").doc(firebaseUser.uid).collection("addChild").doc("OxtF7ijtoOlOX7zx3xcY")
+  /*  const childData= db.collection("usuarios").doc(firebaseUser.uid).collection("addChild").doc("OxtF7ijtoOlOX7zx3xcY")
   console.log("hola", childData.edadChild)
     ///Obteniendo todo el contenido de firebase///*/
-
-
 
   return (
     <div>
       <div className="background-tips-sesamo" id="aprendamos-page">
         <div className=" animated fadeIn">
-            <>
-            <a onClick={()=>{history.goBack()}}>
-              <i  className="fas fa-chevron-left arrowGoback" aria-hidden="true"></i>
-            </a> 
-              {arrTips
-                .filter(
-                  (item) =>
-                    item.seccion ==="Sésamo" &&
-                    item.n_tip == localStorage.getItem('localNumberTip')
-                )
-                .map((item) => (
-                  <div className="">
-                    <div className="row justify-content-center h-100 sin-margin-row">
-                      <h1 className="tittle-tips"> {item.titulo}</h1>
-                    </div>
-                    <div className="row justify-content-center  h-100 sin-margin-row">
-                      <h3 className="tips-numbers"> {item.tipo}</h3>
-                    </div>
-                    <div className="row tips-margin-top sin-margin-row">
-                      <div className="col-sm-12 col-md-12 col-lg-7 col-xl-7">
-                        <div className="videoWrapper">
-                          <iframe
-                            className="border-video-rutina"
-                            width="600"
-                            height="350"
-                            src={item.video}
-                            frameborder="0"
-                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen
-                          ></iframe>
-                        </div>
+          <>
+            <a
+              onClick={() => {
+                history.goBack();
+              }}
+            >
+              <i
+                className="fas fa-chevron-left arrowGoback"
+                aria-hidden="true"
+              ></i>
+            </a>
+            {arrTips
+              .filter(
+                (item) =>
+                  item.seccion === "Sésamo" &&
+                  item.n_tip == localStorage.getItem("localNumberTip")
+              )
+              .map((item) => (
+                <div className="">
+                  <div className="row justify-content-center h-100 sin-margin-row">
+                    <h1 className="tittle-tips"> {item.titulo}</h1>
+                  </div>
+                  <div className="row justify-content-center  h-100 sin-margin-row">
+                    <h3 className="tips-numbers"> {item.tipo}</h3>
+                  </div>
+                  <div className="row tips-margin-top sin-margin-row">
+                    <div className="col-sm-12 col-md-12 col-lg-7 col-xl-7">
+                      <div className="videoWrapper">
+                        <iframe
+                          className="border-video-rutina"
+                          width="600"
+                          height="350"
+                          src={item.video}
+                          frameborder="0"
+                          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                          allowfullscreen
+                        ></iframe>
                       </div>
-                      <div className="col-sm-12 col-md-12 col-lg-5 col-xl-5">
-                        <div class="text-tips">
-                          <p class="text-tips-light">
-                          {item.texto1}
-                          </p>
-                          <p class="text-tips-light">
-                          {item.texto2}
-                          </p>
-                          <p class="text-tips-light">
-                          {item.texto3}
-                          </p>
-                          <p class="text-tips-light">
-                          {item.texto4}
-                          </p>
-                          <p class="text-tips-light">
-                          {item.texto5}
-                          </p>
-                          <p class="text-tips-light">
-                          {item.texto6}
-                          </p>
-                          <p class="text-tips-bold">
-                          </p>
-                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-12 col-lg-5 col-xl-5">
+                      <div class="text-tips">
+                        <p class="text-tips-light">{item.texto1}</p>
+                        <p class="text-tips-light">{item.texto2}</p>
+                        <p class="text-tips-light">{item.texto3}</p>
+                        <p class="text-tips-light">{item.texto4}</p>
+                        <p class="text-tips-light">{item.texto5}</p>
+                        <p class="text-tips-light">{item.texto6}</p>
+                        <p class="text-tips-bold"></p>
                       </div>
                     </div>
                   </div>
-                ))}
-            </>
+                </div>
+              ))}
+          </>
         </div>
       </div>
     </div>
---
+++ src/components/loginRegister/Condiciones.jsx
@@ -0,0 +1,198 @@
+import React from "react";
+
+const Condiciones = () => {
+  return (
+    <div className="container mt-4  animated fadeIn ">
+      <div className="register-child contactanos-box">
+        <h1 className="title tittle-register-child text-contactanos tracking-in-expand-fwd-top">
+          CONDICIONES DE USO
+        </h1>
+        <div className="list-register">
+          <h5> 10 de Septiembre 2020</h5>
+          <p>
+            POR FAVOR LEA ATENTAMENTE ESTOS TÉRMINOS DE USO. ESTOS TÉRMINOS
+            CONSTITUYEN UN ACUERDO VINCULANTE ENTRE USTED Y VOLAR, PARA DEL
+            SITIO WEB DE VOLAR. AL DESCARGAR, UTILIZAR O ACCEDER AL SITIO WEB,
+            USTED ACEPTA ESTAR LEGALMENTE OBLIGADO POR TODOS LOS TÉRMINOS Y
+            CONDICIONES CONTENIDOS EN ESTOS TÉRMINOS.
+          </p>
+          <p>
+            Volar se reserva el derecho de cambiar o modificar cualquiera de los
+            términos y condiciones contenidos en estos Términos de uso en
+            cualquier momento y a su sola discreción, notificando que los
+            Términos de uso han sido modificados. Dicho aviso puede
+            proporcionarse revisando la fecha en la parte superior de estos
+            Términos de uso o por cualquier otra forma de aviso según lo
+            determinado por Volar. Su uso continuado de la Aplicación o los
+            Sitios después de la publicación de los Términos de uso revisados
+            ​​u otro aviso de dichos cambios confirmará su aceptación de dichos
+            cambios o modificaciones.
+          </p>
+          <h4> Política de privacidad</h4>
+          <p>
+            Consulte nuestra Política de privacidad para obtener más información
+            sobre cómo Volar recopila, usa, almacena y divulga datos técnicos,
+            información personal e información relacionada en relación con el
+            sitio Web.
+          </p>
+          <h4>Copyright</h4>
+          <p>
+            La web de volar y otros materiales incluidos entre otros el logotipo
+            de Volar y todos los diseños, textos, información, datos, software,
+            archivos de sonido, otros archivos y la selección y disposición de
+            los mismos.son propiedad exclusiva de Volar.
+          </p>
+
+          <h4> Licencia de usuario final</h4>
+          <p>
+            Sujeto a los términos, condiciones y limitaciones establecidos en
+            estos Términos de uso, Volar le otorga una licencia no exclusiva e
+            intransferible (la "Licencia") para descargar y usar la Aplicación
+            en cualquier dispositivo móvil que posea o controle y para acceder a
+            los Sitios y los Materiales de Volar, únicamente para su uso
+            informativo, no comercial y personal. Dicha Licencia está sujeta a
+            estos Términos de uso y, salvo que se indique expresamente lo
+            contrario en estos Términos de uso,no incluye: (a) reventa o uso
+            comercial; (b) la reproducción, distribución, presentación pública o
+            exhibición pública de la Aplicación, los Sitios o cualquier Material
+            de VOLAR, excepto según lo expresamente permitido por Volar; (c)
+            modificar o hacer cualquier uso derivado de la Aplicación, el Sitio
+            y los Materiales de Volar, o cualquier parte de los mismos; (d) uso
+            de cualquier minería de datos, robots o métodos similares de
+            recopilación o extracción de datos; o (e) cualquier uso de la
+            Aplicación, los Sitios o los Materiales de Volar que no sea para el
+            propósito previsto.
+          </p>
+
+          <h4>Uso del contenido de Volar</h4>
+          <p>
+            Los Sitios pueden contener áreas que indiquen que incluyen consejos
+            de Volar y otro contenido de Volar destinado a ser descargado por
+            los usuarios o pueden vincularse a áreas fuera del sitio en las que
+            se almacenan dichos materiales. Está autorizado a descargar,
+            reproducir y distribuir dicho Contenido de Volar siempre que: (a)
+            estén específicamente designados como para su descarga; (b) se
+            utilizan únicamente con fines no comerciales; (c) no se editan ni
+            modifican de ninguna manera; (d) los videos no se pueden descargar,
+            reproducir, pero pueden estar vinculados y (e) se usan de acuerdo
+            con las reglas especiales designadas en los Sitios que se aplican al
+            uso de dicho contenido.
+          </p>
+          <h4> Quejas por derechos de autor</h4>
+          <p>
+            Si cree que algo en el Sitio infringe los derechos de autor que
+            posee o controla, puede presentar una notificación de dicha
+            infracción en la sección de contáctanos.
+          </p>
+          <h4>Marcas comerciales</h4>
+          <p>
+            VOLAR, los logotipos de Volar y cualquier otro nombre o eslogan de
+            producto o servicio de Volar contenido en la web son marcas
+            comerciales de Volar y no pueden copiarse, imitarse ni utilizarse,
+            en su totalidad o en parte, sin el permiso previo por escrito de
+            Volar. o el titular de la marca comercial correspondiente. Además,
+            la apariencia de la Aplicación y los Sitios es la marca de servicio,
+            la marca comercial y / o la imagen comercial de Volar y no se puede
+            copiar, imitar ni usar, en su totalidad o en parte, sin nuestro
+            permiso previo por escrito.
+          </p>
+          <h4>Sitios y servicios de terceros; Materiales de terceros.</h4>
+          <p>
+            La Aplicación y los Sitios pueden proporcionar enlaces a sitios web
+            de terceros, aplicaciones, servicios móviles u otros productos o
+            servicios de terceros ("Productos y servicios de terceros") y
+            también pueden mostrar, vincular o poner a disposición contenido,
+            datos, información, eventos, publicidad, aplicaciones o materiales
+            ("Materiales de terceros"). Vroom no respalda ni controla y no hace
+            declaraciones ni garantías de ningún tipo con respecto a los
+            Productos y Servicios de Terceros o Materiales de Terceros, incluso
+            con respecto al contenido, precisión, calidad, naturaleza,
+            idoneidad, decencia, funcionalidad, rendimiento, confiabilidad,
+            integridad, actualidad, vigencia, seguridad, legalidad o cualquier
+            otro aspecto de los mismos. Sus tratos comerciales o correspondencia
+            con terceros, y cualquier término, condición, las garantías o
+            representaciones asociadas con dichos tratos o promociones son
+            únicamente entre usted y dicho tercero. Si accede o utiliza un sitio
+            web de terceros, debe tener en cuenta que los términos y políticas
+            de Volar ya no rigen. Debe revisar los términos y políticas
+            aplicables, incluidas las prácticas de privacidad y recopilación de
+            datos, de cualquier sitio web de terceros al que navegue desde la
+            Aplicación.
+          </p>
+          <h4>Marcas comerciales</h4>
+          <p>
+            VOLAR, los logotipos de Volar y cualquier otro nombre o eslogan de
+            producto o servicio de Volar contenido en la web son marcas
+            comerciales de Volar y no pueden copiarse, imitarse ni utilizarse,
+            en su totalidad o en parte, sin el permiso previo por escrito de
+            Volar. o el titular de la marca comercial correspondiente. Además,
+            la apariencia de la Aplicación y los Sitios es la marca de servicio,
+            la marca comercial y / o la imagen comercial de Volar y no se puede
+            copiar, imitar ni usar, en su totalidad o en parte, sin nuestro
+            permiso previo por escrito.
+          </p>
+          <h4>Contenido y conducta del usuario</h4>
+          <p>
+            Los usuarios son los únicos responsables de todas las fotografías,
+            mensajes, comentarios y otro contenido ("Contenido del usuario") que
+            creen, publiquen o transmitan utilizando el sitio web, o que se
+            publiquen o transmitan utilizando su cuenta, así como de su conducta
+            y la conducta de cualquiera que use su cuenta.
+          </p>
+          <h4> Datos de registro; Seguridad de la cuenta</h4>
+          <p>
+            En consideración a su uso del sitio web, usted acepta (a)
+            proporcionar información precisa, actual y completa sobre usted
+            según lo solicite cualquier formulario de registro en el Sitio
+            ("Datos de registro"); (b) mantener la seguridad de su contraseña e
+            identificación; (c) mantener y actualizar de inmediato los Datos de
+            registro y cualquier otra información que proporcione a Vroom, para
+            mantenerla precisa, actualizada y completa; y (d) aceptar todos los
+            riesgos de acceso no autorizado a los Datos de registro y cualquier
+            otra información que proporcione a Volar.
+          </p>
+          <h4>Descargo de responsabilidad</h4>
+          <p>
+            Volarse reserva el derecho de cambiar todo el contenido de la
+            Aplicación o los Sitios y de modificar, suspender o interrumpir la
+            Aplicación, los Sitios o cualquier característica o funcionalidad de
+            los mismos en cualquier momento sin previo aviso y sin obligación ni
+            responsabilidad hacia usted. La referencia a cualquier producto,
+            servicio, proceso u otra información, por nombre comercial, marca
+            comercial, fabricante, proveedor o de otro modo, no constituye ni
+            implica respaldo, patrocinio o recomendación de los mismos, ni
+            ninguna afiliación con ellos, por parte de Vroom.
+          </p>
+          <h4>Ley aplicable y jurisdicción</h4>
+          <p>
+            Estos Términos de uso y su uso del sitio Web se regirán e
+            interpretarán de acuerdo con las leyes del Estado de Peruano. Usted
+            da su consentimiento irrevocable e incondicional y se somete a la
+            jurisdicción exclusiva de dichos tribunales sobre cualquier demanda,
+            acción o procedimiento que surja de estos Términos de uso.
+          </p>
+          <h4>Terminación</h4>
+          <p>
+            Sin perjuicio de cualquiera de estos Términos de uso, Volar se
+            reserva el derecho, sin previo aviso y a su sola discreción, de
+            rescindir su licencia para usar el sio Web, y de bloquear o prevenir
+            su acceso y uso futuro del Sitio.
+          </p>
+          <h4>Divisibilidad</h4>
+          <p>
+            Si alguna disposición de estos Términos de uso se considera ilegal,
+            nula o inaplicable por cualquier motivo, dicha disposición se
+            considerará separable de estos Términos de uso y no afectará la
+            validez y aplicabilidad de las disposiciones restantes.
+          </p>
+          <h4>Preguntas e información de contacto</h4>
+          <p>
+          Las preguntas o comentarios sobre el Sitio pueden dirigirse a la sección de contáctanos.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Condiciones;
